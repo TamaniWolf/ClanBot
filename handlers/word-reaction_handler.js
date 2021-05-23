421aeec8440095b1d28c6a54e13439c05b1bb9f1
Tamani Wolf <VinylWolf.mail@gmail.com>
2021-05-23 04:13:23 +0200

    Create word-reaction_handler.js

---
+++ handlers/word-reaction_handler.js
@@ -0,0 +1,18 @@
+const lang = require('../lang/en_US.json');
+const chalk = require('chalk');
+const fs = require('fs');
+var moment = require('moment');
+
+module.exports = (client, Discord) =>{
+    const command_files = fs.readdirSync('./commands/word-reaction/').filter(file => file.endsWith('.js'));
+
+    for(const file of command_files){
+        const command = require(`../commands/word-reaction/${file}`);
+        if(command.name){
+            client.commands.set(command.name, command);
+        } else {
+            continue;
+        }
+    }
+    console.log(chalk.yellow('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '][Discord]', chalk.white('word-reaction Heandler loaded')));
+}
