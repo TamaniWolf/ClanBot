99506c2312d4da8c32357dcb040febd242593e81
Tamani Wolf <VinylWolf.mail@gmail.com>
2021-05-23 04:40:01 +0200

    Create add.js

---
+++ commands/admin/add.js
@@ -0,0 +1,11 @@
+const lang = require('../../lang/en_US.json');
+const chalk = require('chalk');
+const configonoff = require('../../config/onoff.json');
+const channelconfig = require('../../config/channels.json');
+const roleconfig = require('../../config/roles.json');
+var moment = require('moment');
+require('dotenv').config();
+
+if(configonoff.command.admin.add === true) {
+    
+}
