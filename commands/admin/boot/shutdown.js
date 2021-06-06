c379a431fada91b790593bc08903586022930a15
Tamani Wolf <VinylWolf.mail@gmail.com>
2021-06-06 04:46:26 +0200

    Update shutdown.js

--- commands/admin/boot/shutdown.js
+++ commands/admin/boot/shutdown.js
@@ -1,8 +1,9 @@
-const lang = require('../../../lang/en_US.json');
-const chalk = require('chalk');
+const configmain = require('../../../config/config.json');
+const configchannel = require('../../../config/channels.json');
 const configonoff = require('../../../config/onoff.json');
-const channelconfig = require('../../../config/channels.json');
-const roleconfig = require('../../../config/roles.json');
+const configrole = require('../../../config/roles.json');
+const lang = require('../../.' + configmain.lang);
+const chalk = require('chalk');
 var moment = require('moment');
 require('dotenv').config();
 
@@ -14,19 +15,22 @@ module.exports = {
     guildOnly: true,
     async execute(client, message, args) {
         if(configonoff.command.admin.boot.shutdown === true) {
-            const adminchannel1 = channelconfig.admin.admin1;
-            const adminchannel2 = channelconfig.admin.admin2;
-            const adminchannel3 = channelconfig.admin.admin3;
-            const admin1 = roleconfig.admin.admin1;
-            const admin2 = roleconfig.admin.admin2;
-            const admin3 = roleconfig.admin.admin3;
-            if(message.channel.id === adminchannel1 || message.channel.id === adminchannel2 || message.channel.id === adminchannel3) {
-                if (message.member.roles.cache.has(admin1) || message.member.roles.cache.has(admin2) || message.member.roles.cache.has(admin3)) {
-                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, chalk.white(lang.admin.shutdown.log)));
+            const adminchannel1 = configchannel.admin.admin1;
+            const adminchannel2 = configchannel.admin.admin2;
+            const adminchannel3 = configchannel.admin.admin3;
+            const adminchannel4 = configchannel.admin.admin4;
+            const adminchannel5 = configchannel.admin.admin5;
+            const admin1 = configrole.admin.admin1;
+            const admin2 = configrole.admin.admin2;
+            const admin3 = configrole.admin.admin3;
+            const admin4 = configrole.admin.admin4;
+            const admin5 = configrole.admin.admin5;
+            if(message.channel.id === adminchannel1 || message.channel.id === adminchannel2 || message.channel.id === adminchannel3 || message.channel.id === adminchannel4 || message.channel.id === adminchannel5) {
+                if(message.member.roles.cache.has(admin1) || message.member.roles.cache.has(admin2) || message.member.roles.cache.has(admin3) || message.member.roles.cache.has(admin4) || message.member.roles.cache.has(admin5)) {
+                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, chalk.white(lang.admin.shutdown.log)));
                     message.channel.send(lang.admin.shutdown.text)
                     const { exec } = require("child_process");
-
-                    exec("pm2 stop royalbot", (error, stdout, stderr) => {
+                    exec("pm2 stop clanBot", (error, stdout, stderr) => {
                         if (error) {
                             console.log(`error: ${error.message}`);
                             return;
@@ -38,10 +42,10 @@ module.exports = {
                         console.log(`stdout: ${stdout}`);
                     });
                 } else {
-                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, chalk.white(lang.admin.shutdown.errorperms)));
+                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, chalk.white(lang.admin.shutdown.errorperms)));
                 }
             } else {
-                console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, chalk.white(lang.admin.shutdown.errorchannel)));
+                console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, chalk.white(lang.admin.shutdown.errorchannel)));
             }
         }
     }
