a6729dbac7f0ce1920684b1041584c3a31ff3c53
Tamani Wolf <VinylWolf.mail@gmail.com>
2021-06-06 04:40:41 +0200

    Update addprofile.js

--- commands/user/coin/addprofile.js
+++ commands/user/coin/addprofile.js
@@ -1,5 +1,8 @@
-const lang = require('../../../lang/en_US.json');
+const configmain = require('../../../config/config.json');
+const configchannel = require('../../../config/channels.json');
 const configonoff = require('../../../config/onoff.json');
+const configrole = require('../../../config/roles.json');
+const lang = require('../../.' + configmain.lang);
 const chalk = require('chalk');
 const fs = require('fs');
 var moment = require('moment');
@@ -23,16 +26,16 @@ module.exports = {
             const admin4 = configrole.admin.admin4;
             const admin5 = configrole.admin.admin5;
             if(message.channel.id === adminchannel1 || message.channel.id === adminchannel2 || message.channel.id === adminchannel3 || message.channel.id === adminchannel4 || message.channel.id === adminchannel5) {
-                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, chalk.white(lang.admin.help.log)));
+                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, chalk.white(lang.admin.help.log)));
                         const member = message.member
                     
                     if (configonoff.command.user.eco.addprofile === true) {
-                        // console.log(lang.prefix.royal, `UserID ${member.id}, ${member.displayName}`)
+                        // console.log(lang.prefix.clan, `UserID ${member.id}, ${member.displayName}`)
                         // profile to check if exists
                         const profile = `./db/economy/profiles/${member.id}.json`;
                         // check if profile exists
                         if (fs.existsSync(profile)) {
-                            console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, `Rejoined '${member.displayName} : ${member.id}'.`));
+                            console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, `Rejoined '${member.displayName} : ${member.id}'.`));
                             return;
                         } else {
                             // DB Number count
@@ -47,7 +50,7 @@ module.exports = {
                             let datacount = JSON.stringify(dbnumberwrite, null, 2);
                             fs.writeFileSync('./db/count/dbnumber.json', datacount);
                             // Create new Profile if not exists
-                            // console.log(chalk.cyan('[' + moment().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, `Profile of '${member.displayName}' not found.`));
+                            // console.log(chalk.cyan('[' + moment().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, `Profile of '${member.displayName}' not found.`));
                             let profilewrite = {
                                 dbNumber: dbnumbercount,
                                 version: 1,
@@ -77,7 +80,7 @@ module.exports = {
                             let dataprofile = JSON.stringify(profilewrite, null, 2);
                             fs.appendFile(`./db/economy/profiles/${member.id}.json`, dataprofile, function (err){
                                 if (err) throw err;
-                            console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, `Profile file '${member.id}.json' is creacted.`));
+                            console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, `Profile file '${member.id}.json' is creacted.`));
                             });
                 
                             let newjointime = moment.utc(member.joinedTimestamp).add(4, 'days').format('MM/DD/YYYY-hh:mm:ss-a')
@@ -94,7 +97,7 @@ module.exports = {
                             let datacooldown = JSON.stringify(cooldownwrite, null, 2);
                             fs.appendFile(`./db/cooldown/${member.id}.json`, datacooldown, function (err){
                                 if (err) throw err;
-                            console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, `Cooldown file '${member.is}.json' is creacted.`));
+                            console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, `Cooldown file '${member.is}.json' is creacted.`));
                             });
                         }
                     } else {
@@ -103,7 +106,7 @@ module.exports = {
                 
             } else {
 
-                console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, chalk.white(lang.admin.help.errorchannel)));
+                console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, chalk.white(lang.admin.help.errorchannel)));
             }
         }
     }
