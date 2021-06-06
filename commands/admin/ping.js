96424308a7c142a5b06080d11d63e8dd26908fd4
Tamani Wolf <VinylWolf.mail@gmail.com>
2021-06-06 04:41:25 +0200

    Update ping.js

--- commands/admin/ping.js
+++ commands/admin/ping.js
@@ -1,8 +1,9 @@
-const lang = require('../../lang/en_US.json');
-const chalk = require('chalk');
+const configmain = require('../../config/config.json');
+const configchannel = require('../../config/channels.json');
 const configonoff = require('../../config/onoff.json');
-const channelconfig = require('../../config/channels.json');
-const roleconfig = require('../../config/roles.json');
+const configrole = require('../../config/roles.json');
+const lang = require('../.' + configmain.lang);
+const chalk = require('chalk');
 var moment = require('moment');
 require('dotenv').config();
 
@@ -13,21 +14,25 @@ module.exports = {
     guildOnly: true,
     async execute(client, message, args) {
         if(configonoff.command.admin.ping === true) {
-            const adminchannel1 = channelconfig.admin.admin1;
-            const adminchannel2 = channelconfig.admin.admin2;
-            const adminchannel3 = channelconfig.admin.admin3;
-            const admin1 = roleconfig.admin.admin1;
-            const admin2 = roleconfig.admin.admin2;
-            const admin3 = roleconfig.admin.admin3;
-            if(message.channel.id === adminchannel1 || message.channel.id === adminchannel2 || message.channel.id === adminchannel3) {
-                if (message.member.roles.cache.has(admin1) || message.member.roles.cache.has(admin2) || message.member.roles.cache.has(admin3)) {
+            const adminchannel1 = configchannel.admin.admin1;
+            const adminchannel2 = configchannel.admin.admin2;
+            const adminchannel3 = configchannel.admin.admin3;
+            const adminchannel4 = configchannel.admin.admin4;
+            const adminchannel5 = configchannel.admin.admin5;
+            const admin1 = configrole.admin.admin1;
+            const admin2 = configrole.admin.admin2;
+            const admin3 = configrole.admin.admin3;
+            const admin4 = configrole.admin.admin4;
+            const admin5 = configrole.admin.admin5;
+            if(message.channel.id === adminchannel1 || message.channel.id === adminchannel2 || message.channel.id === adminchannel3 || message.channel.id === adminchannel4 || message.channel.id === adminchannel5) {
+                if(message.member.roles.cache.has(admin1) || message.member.roles.cache.has(admin2) || message.member.roles.cache.has(admin3) || message.member.roles.cache.has(admin4) || message.member.roles.cache.has(admin5)) {
                     message.channel.send(lang.admin.ping.text);
-                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, chalk.white(lang.admin.ping.log)));
+                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, chalk.white(lang.admin.ping.log)));
                 } else {
-                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, chalk.white(lang.admin.ping.errorperms)));
+                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, chalk.white(lang.admin.ping.errorperms)));
                 }
             } else {
-                console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, chalk.white(lang.admin.ping.errorchannel)));
+                console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, chalk.white(lang.admin.ping.errorchannel)));
             }
         }
     }
