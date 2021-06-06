ad99bc2126edefabf599acd0727396f4d819b733
Tamani Wolf <VinylWolf.mail@gmail.com>
2021-06-06 04:42:29 +0200

    Update changelog.js

--- commands/admin/changelog.js
+++ commands/admin/changelog.js
@@ -1,9 +1,10 @@
-const lang = require('../../lang/en_US.json');
+const configmain = require('../../config/config.json');
+const configchannel = require('../../config/channels.json');
+const configonoff = require('../../config/onoff.json');
+const configrole = require('../../config/roles.json');
+const lang = require('../.' + configmain.lang);
 const chalk = require('chalk');
 const fs = require('fs');
-const configonoff = require('../../config/onoff.json');
-const channelconfig = require('../../config/channels.json');
-const roleconfig = require('../../config/roles.json');
 var moment = require('moment');
 require('dotenv').config();
 var hasIcon = 'yes';
@@ -16,14 +17,18 @@ module.exports = {
     guildOnly: true,
     async execute(client, message, args, Discord) {
         if(configonoff.command.admin.changelog === true) {
-            const adminchannel1 = channelconfig.admin.admin1;
-            const adminchannel2 = channelconfig.admin.admin2;
-            const adminchannel3 = channelconfig.admin.admin3;
-            const admin1 = roleconfig.admin.admin1;
-            const admin2 = roleconfig.admin.admin2;
-            const admin3 = roleconfig.admin.admin3;
-            if(message.channel.id === adminchannel1 || message.channel.id === adminchannel2 || message.channel.id === adminchannel3) {
-                if (message.member.roles.cache.has(admin1) || message.member.roles.cache.has(admin2) || message.member.roles.cache.has(admin3)) {
+            const adminchannel1 = configchannel.admin.admin1;
+            const adminchannel2 = configchannel.admin.admin2;
+            const adminchannel3 = configchannel.admin.admin3;
+            const adminchannel4 = configchannel.admin.admin4;
+            const adminchannel5 = configchannel.admin.admin5;
+            const admin1 = configrole.admin.admin1;
+            const admin2 = configrole.admin.admin2;
+            const admin3 = configrole.admin.admin3;
+            const admin4 = configrole.admin.admin4;
+            const admin5 = configrole.admin.admin5;
+            if(message.channel.id === adminchannel1 || message.channel.id === adminchannel2 || message.channel.id === adminchannel3 || message.channel.id === adminchannel4 || message.channel.id === adminchannel5) {
+                if(message.member.roles.cache.has(admin1) || message.member.roles.cache.has(admin2) || message.member.roles.cache.has(admin3) || message.member.roles.cache.has(admin4) || message.member.roles.cache.has(admin5)) {
                     const changelog_files = fs.readdirSync('./db/changelogs/').filter(file => file.endsWith('.json'));
 
                     for(const file of changelog_files){
@@ -41,32 +46,29 @@ module.exports = {
                     client.commands.delete(changeName)
                     const pull = require(`../../db/changelogs/${changeName}.json`)
                     client.commands.set(changeName, pull)
-                    // message.channel.send(`${args[0]}`)
-                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, chalk.white(lang.admin.changelog.log)));
+                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, chalk.white(lang.admin.changelog.log)));
 
                     let rawdata = fs.readFileSync(`./db/changelogs/${args[0]}.json`);
                     let changeRead = JSON.parse(rawdata);
-                    // console.log(changeRead.embed)
 
                     let changeEmbed = changeRead.embed
                     let changeDate = changeRead.date
                     
                     try {
                         if (args[0]) {
-                            
-                            const royalEmbedChangelog = new Discord.MessageEmbed().attachFiles(['./db/image/royal_Evil_Smile.png']).setTitle(
-                                'Royal\'s Changelog from:').setColor(embedColor).setDescription(
-                                `**__${changeDate}__**\n \n   ${changeEmbed}`).setThumbnail('attachment://royal_Evil_Smile.png')
-                            message.channel.send(royalEmbedChangelog);
+                            const clanEmbedChangelog = new Discord.MessageEmbed().attachFiles(['./db/image/Image1.png']).setTitle(
+                                lang.admin.changelog.title).setColor(embedColor).setDescription(
+                                `**__${changeDate}__**\n \n   ${changeEmbed}`).setThumbnail('attachment://Image1.png')
+                            message.channel.send(clanEmbedChangelog);
                         }
                     } catch(e) {
-                        console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, chalk.white(e)));
+                        console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, chalk.white(e)));
                     }
                 } else {
-                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, chalk.white(lang.admin.changelog.errorperms)));
+                    console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, chalk.white(lang.admin.changelog.errorperms)));
                 }
             } else {
-                console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.royal, chalk.white(lang.admin.changelog.errorchannel)));
+                console.log(chalk.cyan('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, chalk.white(lang.admin.changelog.errorchannel)));
             }
         }
     }
