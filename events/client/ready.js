3b70190570b915110d8569a818b42464bfbb176f
Tamani Wolf <VinylWolf.mail@gmail.com>
2022-12-05 22:18:32 +0100

    Delete events directory

--- events/client/ready.js
+++
@@ -1,9 +0,0 @@
-const configmain = require('../../config/config.json')
-const lang = require('../.' + configmain.lang);
-const chalk = require('chalk');
-var moment = require('moment');
-
-module.exports = () =>{
-    //console.log(lang.prefix.discord, `Royalbot is ready; logged in as ${client.user.tag}.`)
-    console.log(chalk.yellow('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + ']' + lang.prefix.clan, chalk.white(lang.log.ready)));
-}
